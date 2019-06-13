6cf65c9001ae6780b7cc61477c721456c109c2b7
hashland <pauldwamena10@gmail.com>
2019-06-13 05:36:13 -0700

    added delete route to  todoController

--- Api/src/TodoController.js
+++ Api/src/TodoController.js
@@ -50,4 +50,77 @@ router.post('/createTodo',auth.checkToken,(req,res)=>{
 
   //updating a Todo
 
+router.put('/update/:id',auth.checkToken,(req,res)=>{
+    let body=req.body
+    Todo.findById(req.params.id,'user title content completed',(err,doc)=>{
+      if(err){console.error(err)}
+      var userID=doc.user._id
+      var authenticateduserid=req.userId
+      if(userID !=authenticateduserid){
+       return res.status(500).send("You do not have acces to edit Todo")
+      }
+      else if(req.body.completed=="true"){
+        Todo.updateOne({_id:req.params.id},{$set:{completed:true},title:req.body.title,description:req.body.description }, function(err,result){
+          if(err){
+            
+            return res.status(500).send("Unable to update Todo")
+          }
+          else{
+            res.json("You have updated Todo successfully")
+          }
+      })
+      }
+      else if(req.body.completed=="false" || req.body.completed==null){
+        Todo.updateOne({_id:req.params.id},{$set:{completed:false},title:req.body.title,description:req.body.description }, function(err,result){
+            if(err){   
+              return res.status(500).send("Unable to update Todo")
+            }
+            else{
+              res.json("You have updated Todo successfully")
+            }
+        })
+
+      }
+      
+    })
+
+})
+    //delete route
+    router.put('/delete/:id', auth.checkToken,(req, res) => {
+
+        Todo.findById(req.params.id,'user',(err,doc)=>{
+            if(err){console.error(err)}
+            var userID=doc.user._id.toString()
+            var authenticateduserid=req.userId
+            if(userID !==authenticateduserid){
+             res.status(500).send("You are not authorised to delete Todo")
+          }else{
+            Todo.remove({
+              _id: req.params.id
+            }, function(err, post){
+              if (err)
+                res.status.send(err)
+              res.json({
+                msg:'You have successfully deleted a todo'
+              })
+            })
+          }
+      
+        })
+        
+      })
+      
+      
+
+
+
+
+
+        
+       
+    
+    
+    
+
+
   module.exports=router
